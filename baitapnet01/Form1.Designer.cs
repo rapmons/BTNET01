c3317fdae44e7d7cc54232021188d6925f5952d0
BINH <77979118+rapmons@users.noreply.github.com>
2021-11-27 10:40:26 +0700

    update

---
+++ baitapnet01/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace baitapnet01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.btok = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.btc = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(75, 93);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(36, 40);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.but_Click);
+            // 
+            // btok
+            // 
+            this.btok.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btok.Location = new System.Drawing.Point(237, 146);
+            this.btok.Name = "btok";
+            this.btok.Size = new System.Drawing.Size(36, 40);
+            this.btok.TabIndex = 1;
+            this.btok.Text = "OK";
+            this.btok.UseVisualStyleBackColor = false;
+            this.btok.Click += new System.EventHandler(this.btok_Click);
+            // 
+            // bt0
+            // 
+            this.bt0.Location = new System.Drawing.Point(237, 207);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(36, 40);
+            this.bt0.TabIndex = 2;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.but_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.Location = new System.Drawing.Point(186, 207);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(36, 40);
+            this.bt9.TabIndex = 3;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.but_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.Location = new System.Drawing.Point(129, 207);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(36, 40);
+            this.bt8.TabIndex = 4;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.but_Click);
+            // 
+            // bt7
+            // 
+            this.bt7.Location = new System.Drawing.Point(75, 207);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(36, 40);
+            this.bt7.TabIndex = 5;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.but_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.Location = new System.Drawing.Point(186, 146);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(36, 40);
+            this.bt6.TabIndex = 6;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.but_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.Location = new System.Drawing.Point(129, 146);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(36, 40);
+            this.bt5.TabIndex = 7;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.but_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(75, 146);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(36, 40);
+            this.bt4.TabIndex = 8;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.but_Click);
+            // 
+            // btc
+            // 
+            this.btc.BackColor = System.Drawing.Color.Gold;
+            this.btc.Location = new System.Drawing.Point(237, 93);
+            this.btc.Name = "btc";
+            this.btc.Size = new System.Drawing.Size(36, 40);
+            this.btc.TabIndex = 9;
+            this.btc.Text = "C";
+            this.btc.UseVisualStyleBackColor = false;
+            this.btc.Click += new System.EventHandler(this.btc_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(186, 93);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(36, 40);
+            this.bt3.TabIndex = 10;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.but_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(129, 93);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(36, 40);
+            this.bt2.TabIndex = 11;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.but_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "SECURITY CODE";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 43);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "ACCESS LOG";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 336);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(303, 147);
+            this.listBox1.TabIndex = 15;
+            // 
+            // battap01_net
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 500);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.btc);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.btok);
+            this.Controls.Add(this.bt1);
+            this.KeyPreview = true;
+            this.Name = "battap01_net";
+            this.Text = "battap01_net";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.battap01_net_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button btok;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button btc;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ baitapnet01/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace baitapnet01
+{
+    public partial class Form1 : Form
+    {
+        private string pass = "2001";
+        public Form1()
+        {
+            InitializeComponent();
+            RunRead();
+        }
+        private void RunRead()
+        {
+            FileInfo theSourceFile = new FileInfo(@"D:\Documents\.net\baitapnet01\102190053.txt");
+
+            StreamReader reader = theSourceFile.OpenText();
+
+            string text = reader.ReadLine();
+
+            while (text != null)
+            {
+                listBox1.Items.Add(text);
+                text = reader.ReadLine();
+
+
+            }
+
+            reader.Close();
+
+        }
+
+        private void w()
+        {
+            FileInfo theSourceFile = new FileInfo(@"D:\Documents\.net\baitapnet01\102190053.txt");
+            StreamWriter writer = new StreamWriter(@"D:\Documents\.net\baitapnet01\102190053.txt", false);
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                writer.WriteLine((string)listBox1.Items[i]);
+            }
+            writer.Close();
+
+        }
+
+        private void but_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (textBox1.TextLength < 4)
+            {
+                textBox1.Text = textBox1.Text + btn.Text;
+
+            }
+
+
+
+        }
+
+        private void btc_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void btok_Click(object sender, EventArgs e)
+        {
+            bt_hehe();
+        }
+        private void bt_hehe()
+        {
+            DateTime d = DateTime.Now;
+            if (textBox1.Text == "2001")
+            {
+                listBox1.Items.Add(d.ToString() + "  Access granted");
+            }
+
+            else if (textBox1.TextLength == 1)
+            {
+                listBox1.Items.Add(d.ToString() + "   Restricted Access ");
+            }
+            else
+            {
+                listBox1.Items.Add(d.ToString() + "  Access denied");
+            }
+
+            w();
+
+        }
+
+
+        private void battap01_net_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar))
+            {
+
+                e.Handled = true;
+
+
+            }
+
+            if (e.KeyChar == 13)
+            {
+
+                bt_hehe();
+
+            }
+            if (e.KeyChar == 8)
+            {
+                textBox1.Text = "";
+            }
+
+
+        }
+
+
+    }
+}
